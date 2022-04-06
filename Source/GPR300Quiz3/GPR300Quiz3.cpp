befff916a89de15dc428d884ed50a1f2e250aa9d
EliMooreChamplain <eli.moore@mymail.champlain.edu>
2022-04-06 19:47:17 -0400

    Initial Commit

---
+++ Source/GPR300Quiz3/GPR300Quiz3.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "GPR300Quiz3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GPR300Quiz3, "GPR300Quiz3" );
+ 
---
+++ Source/GPR300Quiz3Editor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GPR300Quiz3EditorTarget : TargetRules
+{
+	public GPR300Quiz3EditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("GPR300Quiz3");
+	}
+}
